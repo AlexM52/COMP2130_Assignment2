1cdf3e76f992f372e43dc1e8a8cb13403775a0a0
Alex <alexmcdoom@hotmail.com>
2016-04-08 03:39:16 -0500

    Basic client-server pair.

---
+++ spreadsheet_client.c
@@ -0,0 +1,69 @@
+/**
+@filename:	spreadsheet_server.c
+@author:	vwainne(), Alex McDoom(62-0025254)
+@course:	COMP2130 (Systems Programming)
+@lecturer:	Kevin Miller
+@dateDue:	April 14, 2016
+@task:	Assignment 2 - Implement spreadsheet server	
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+// #define BUF_SIZE	1024
+#define	SERVER_IP	"127.0.0.1"
+#define SERVER_PORT	60000
+
+int main(int argc, char *argv[])
+{
+	int	sock, r;
+    struct sockaddr_in	dest_addr;
+    // const int BUF_SIZE = 1024;
+    // const char SERVER_IP[] = "127.0.0.1";
+    // const int SERVER_PORT = 60000;
+    char msg[50];//, buf[BUF_SIZE];
+    int	send_len, bytes_sent;
+
+    // create socket
+    sock=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock < 0){
+        printf("Could not create socket.\n");
+        exit(0);
+    }
+
+    // set up socket address struct
+    memset(&dest_addr, 0, sizeof (dest_addr));
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    dest_addr.sin_port = htons((unsigned short)SERVER_PORT);
+
+    // connect to the server
+    r = connect(sock, (struct sockaddr *) &dest_addr, sizeof(dest_addr));
+    if (r < 0){
+        printf("Could not connect to server.\n");
+        exit(0);
+    }
+
+    // main loop
+    while (1){
+        printf(">> ");
+        // scanf("%s", msg);
+        fgets(msg, 19, stdin);
+        // printf("%s\n", msg);
+        if (strncmp(msg ,"quit", 4) == 0)
+            break;
+        // strcpy(buf,text);
+        send_len = strlen(msg);
+        // bytes_sent=send(sock_send, buf, send_len,0);
+        bytes_sent = send(sock, msg, send_len, 0);
+    }
+
+    // close socket and exit
+    printf("Closing socket...\n");
+    close(sock);
+    printf("DONE.\n");
+	return 0;
+}
---
+++ spreadsheet_server.c
@@ -0,0 +1,106 @@
+/**
+@filename:	spreadsheet_server.c
+@author:	vwainne(), Alex McDoom(62-0025254)
+@course:	COMP2130 (Systems Programming)
+@lecturer:	Kevin Miller
+@dateDue:	April 14, 2016
+@task:	Assignment 2 - Implement spreadsheet server	
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	int	sock_main, sock_cli, r, addr_size;
+    struct sockaddr_in	my_addr, cli_addr;
+    const unsigned short PORT = 60000;
+    const int BUF_SIZE = 1024;
+    int rcvd_bytes;
+    char in_buffer[BUF_SIZE];
+
+
+    // set up listening socket
+    sock_main=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock_main < 0){
+        printf("Could not create socket.\n");
+        exit(0);
+    }
+    // set up local socket struct
+    memset(&my_addr, 0, sizeof(my_addr));
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    my_addr.sin_port = htons(PORT);
+    // bind to port
+    // int r;
+    r=bind(sock_main, (struct sockaddr *) &my_addr, sizeof (my_addr));
+    if (r < 0){
+        printf("Could not bind to port %d.\n", PORT);
+        exit(0);
+    }
+    // start listening
+    r=listen(sock_main, 5);
+    if (r < 0){
+        printf("Could not listen on main socket.\n");
+        exit(0);
+    }
+
+    // set up socket to manage client
+    addr_size=sizeof(cli_addr);
+    sock_cli=accept(sock_main, (struct sockaddr *) &cli_addr, &addr_size);
+
+    // handle client requests
+    char *args[5];
+    char *t;
+    int n;
+    int id;
+    while(1)
+    {
+    	rcvd_bytes = recv(sock_cli, in_buffer, BUF_SIZE, 0);
+    	in_buffer[rcvd_bytes]=0;
+        printf("Received: %s\n", in_buffer);
+        args[0] = strtok(in_buffer, " \n\0");
+	    n = 1;
+	    while(t = strtok(NULL, " \n\0"))
+	    {
+	    	args[n] = t;
+	    	n++;
+	    }
+	    args[n] = NULL;
+	    // if (strcmp(in_buffer, "finish") == 0)
+	    //     break;
+	    if (strcmp(args[0], "finish") == 0)
+	        break;
+	    // args[0] = strtok(in_buffer, " \n\0");
+	    // n = 1;
+	    // while(t = strtok(NULL, " \n\0"))
+	    // {
+	    // 	args[n] = t;
+	    // 	n++;
+	    // }
+	    // // args[0] = in_buffer;
+	    // // args[1] = NULL;
+	    // args[n] = NULL;
+	    id = fork();
+	    if(id == 0)
+	    {
+	    	execvp(args[0], args);
+	    }else
+	    {
+	    	wait();
+	    	printf("\n");
+	    }
+    }
+
+    // close sockets and exit
+    printf("Closing client socket...\n");
+    close(sock_cli);
+    printf("Closing main socket...\n");
+    close(sock_main);
+    printf("DONE.\n");
+	return 0;
+}
